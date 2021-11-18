2369a22de76df4c3cf4aca9938a56147ee1fa814
Guillermo Sosa <g.sosa@correo.unimet.edu.ve>
2021-11-17 20:30:18 -0400

    interfaz con logica

--- Menu.py
+++ Menu.py
@@ -20,7 +20,7 @@ def menu():
     print('[1] Iniciar GPS\n')
     print('[2] Sobre nosotros\n')
     print('[3] Contáctanos\n')
-    print('[0] Cerrar GPS Cupidon\n')
+    print('[0] Cerrar GPS Cupido\n')
 
 
 def gps():
@@ -45,70 +45,56 @@ def contact():
     print('')
     print('>>>------CONTACTANOS------->\n')
     name = input("Ingrese su nombre y apellido: \n")
-    email = input("Ingrese su correo electrónico: \n")
-    content = input("¿Cuál es el motivo de su mensaje?: \n")
-    print('\n\n¿Deseas enviar este mensaje?')
+    email = input("\nIngrese su correo electrónico: \n")
+    content = input("\n¿Cuál es el motivo de su mensaje?: \n")
+    print('\n\n¿Deseas enviar este mensaje?\n')
     print('De:', name, ': <', email, '>')
     print('Para: GPS Cupido\n')
     print(content)
     print('\n[1] Enviar\n')
 
-header()
-menu()
-optionMenu = int(input("Ingrese su opción deseada: \n"))
+def credits():
+    print('\nProyecto 1 - Modelación de Sistemas en Redes')
+    print('Profesor:')
+    print('     >-Rafael Matienzo->')
+    print('\nIntegrantes:')
+    print('     >-Oriana González->')
+    print('     >-Luis Stanislao->')
+    print('     >-Guillermo Sosa->')
+    print('\nGPS Cupido finalizado\n')
 
-while optionMenu != 0:
+def routes():
+    print('_______________________________________________________________________________')
+    print()
+    print('      ////////     ///     ///    ////////      //////       /////////')
+    print('      //    //     ///     ///      ///        ///////       //')
+    print('      ///////      ///     ///      ///       ///   ///       ////')
+    print('      //   ///     ///     ///      ///      ///////////         ////')
+    print('      //    ///    ///     ///      ///     ///       ///           //')
+    print('      //     ///    /////////       ///    ///         ///   /////////')
+    print('_______________________________________________________________________________')
+    print()
 
-    if optionMenu == 1:
-        gps()
-        optionGps = int(input("Ingrese su opción deseada: \n"))
+def aboutHeader():
+    print('_______________________________________________________________________________')
+    print()
+    print('          //////       //////////      /////////    ///     ///   //////////')
+    print('         ////////      ///     ///    ///     ///   ///     ///      ///')
+    print('        ///   ///      ///////////    ///     ///   ///     ///      ///')
+    print('       ///////////     ///     ///    ///     ///   ///     ///      ///')
+    print('      ///       ///    ///     ///    ///     ///   ///     ///      ///')
+    print('     ///         ///   /////////       /////////     /////////       ///')
+    print('_______________________________________________________________________________')
+    print()
 
-        while optionGps != 0:
-            if optionGps == 1:
-                print('Discoteca The Darkness: Carrera 14 con Calle 50')
-            elif optionGps == 2:
-                print('[2] Bar La Pasión: Calle 54 con Carrera 11\n')
-            elif optionGps == 3:
-                print('[3] Cervecería Mi Rolita: Calle 50 con Carrera 12\n')
-            else:
-                print('Opción invalida\n')
-            gps()
-            optionGps = int(input("Ingrese su opción deseada: \n"))
-
-    elif optionMenu == 2:
-        aboutUs()
-        optionAboutUs = int(input("Ingrese su opción deseada: \n"))
-
-        while optionAboutUs != 0:
-            print('Opción invalida\n')
-            aboutUs()
-            optionAboutUs = int(input("Ingrese su opción deseada: \n"))
-
-    elif optionMenu == 3:
-        contact()
-        optionContact = int(input("Ingrese su opción deseada: \n"))
-
-        while optionContact != 1:
-            print('Opción invalida\n')
-            optionContact = int(input("Ingrese su opción deseada: \n"))
-        print('______________________________________________________________________________')
-        print('|¡Gracias por tu mensaje! Te contactaremos en cuanto llegue la luz al cuarto  |\n')
-        print('|de servidores. ¡Por parte del equipo de GPS Cupido te deseamos un feliz día! |\n')
-        print('|_____________________________________________________________________________|')
-
-    else:
-        print('Opción invalida\n')
-    menu()
-    optionMenu = int(input("Ingrese su opción deseada: \n"))
-
-
-print('\nProyecto 1 - Modelación de Sistemas en Redes')
-print('Profesor Rafael Matienzo')
-print('Integrantes:')
-print('     Oriana González')
-print('     Luis Stanislao')
-print('     Guillermo Sosa')
-print('\nGPS Cupido finalizado\n')
-
-
-
+def contactHeader():
+    print('_______________________________________________________________________________')
+    print()
+    print(' /////////    /////////    ///    ///       //////      //////////  //////////')
+    print(' //    //    ///     ///   ////    //      ////////     //     //      ///')
+    print(' //          ///     ///   // //   //     ///   ///     //             ///')
+    print(' //          ///     ///   //   // //    ///////////    //             ///')
+    print(' //     //   ///     ///   //    ////   ///       ///   //     //      ///')
+    print(' ////////     /////////    //     ///  ///         ///  /////////      ///')
+    print('_______________________________________________________________________________')
+    print()
